df2fa1ba9771bd8f18cfa64439324205359595f8
JakubTlak <marbixxd@gmail.com>
2023-10-12 13:58:10 +0200

    db of exercises and trainings done in file

---
+++ src/main/java/com/example/elgrande/exercise_model/TrainingService.java
@@ -0,0 +1,4 @@
+package com.example.elgrande.exercise_model;
+
+public class TrainingService {
+}
--- src/main/java/com/example/elgrande/exercise_model/model/Training.java
+++ src/main/java/com/example/elgrande/exercise_model/model/Training.java
@@ -3,10 +3,14 @@ package com.example.elgrande.exercise_model.model;
 import com.example.elgrande.exercise_model.enums.Body;
 import com.example.elgrande.exercise_model.enums.Level;
 
+import java.util.List;
+
 public class Training {
     private String name;
     private Body bodyParts;
     private Level level;
+
+    private List<Exercise> exercises;
 
     public Training(String name, Body bodyParts, Level level) {
         this.name = name;
