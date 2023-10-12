58e58098dc58e562f003018cc163ec7b19cfd598
Bart Pra <prabarbarr@gmail.com>
2023-10-12 13:54:18 +0200

    testing endpoints added

---
+++ src/main/java/com/example/elgrande/controller/ExerciseAndDietController.java
@@ -0,0 +1,22 @@
+package com.example.elgrande.controller;
+
+import com.example.elgrande.model.User;
+import com.example.elgrande.model.enums.UserLevel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExerciseAndDietController {
+
+    // ---- to są testowe endpointy.
+    @GetMapping("main")
+    public User displayUser(){
+        return new User("Bart", "123", 78, 190, UserLevel.INTERMEDIATE);
+    }
+
+    @GetMapping("bmi")
+    public double displayBmi(){
+        return new User("Bart", "123", 78, 190, UserLevel.INTERMEDIATE). getBMI();
+    }
+
+}
