f9451b9b34c56f794b4feb0068e4bef214722d0a
Michał Wawro <mwawro77@gmail.com>
2023-12-02 17:13:34 +0100

    Some more work/small changes in react html, css for header and footer in their own css files, added options for using future user information in WelcomePageLoggedIn

--- client/src/Components/WelcomePage/WelcomePageLoggedIn.js
+++ client/src/Components/WelcomePage/WelcomePageLoggedIn.js
@@ -1,14 +1,42 @@
 //import??
 
-const WelcomePageLoggedIn = ({}) => {
-    return(
-        <div>
-            <h1>Zalogowany użytkownik etc etc.</h1>
-            <h2>if ma wszystko wypelnione to diety i traningi</h2>
-            <h3>if nie ma, to ma pokazane buttonsy do powypełniania</h3>
-            <h3>tam, lets start by choosing your training and diets!</h3>
+const usersArray = [{
+    name: 'User1',
+    trainings: ['Training1', 'Training2', 'Training3'],
+    diets: ['Diet1', 'Diet2', 'Diet3']
+}, {
+    name: 'User2',
+    trainings: ['Training1', 'Training2', 'Training3'],
+    diets: []
+}, {
+    name: 'User3',
+    trainings: [],
+    diets: []
+}];
+
+const WelcomePageLoggedIn = ({ testUserNumber }) => {
+    const user = usersArray[testUserNumber];
+    return (
+        <div className="content">
+            <h1 className="BorderedRubik">Hello {user.name}</h1>
+            <div className="MainButtonContainer">
+                {user.trainings.length > 0 ?
+                    <button id='ChoiceButton' className="MainButton" type="button">Your next training is: {user.trainings[1]}</button>
+                    // Tutaj router do strony z detalami kolejnego treningu
+                    :
+                    <button id='ChoiceButton' className="MainButton" type="button">Let's choose a training!</button>
+                    //Tutaj router do strony, która pomaga wybrać training
+                }
+                {user.diets.length > 0 ?
+                    <button id='ChoiceButton' className="MainButton" type="button">Your next meal is: {user.diets[1]}</button>
+                    // Tutaj router do strony z detalami kolejnego posiłku
+                    :
+                    <button id='ChoiceButton' className="MainButton" type="button">Let's choose a diet!</button>
+                    //Tutaj router do strony, która pomaga wybrać dietę
+                }
+            </div>
         </div>
-        
+
     );
 };
 
