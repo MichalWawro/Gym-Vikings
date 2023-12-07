7cc70a470919adcd8244e19f6f8f18a70aa62579
piotrekkn <piotrknetka@gmail.com>
2023-12-07 18:42:21 +0100

    added login component + home button

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,12 +1,17 @@
 import './NavBar.css';
-
+import { useNavigate } from 'react-router-dom';
 //Components
 import NavBarLoggedIn from './NavBarLoggedIn.js';
 import NavBarLoggedOut from './NavBarLoggedOut.js';
 
 const NavBar = ({ isLoggedIn, handleLoginChange }) => {
+    const navigate = useNavigate();
     return (
         <div>
+            <button id="AboutButton" className="HomeButton" type="button" onClick={() => navigate("")}>
+
+            </button>
+
             {isLoggedIn ?
                 <NavBarLoggedIn handleLoginChange={handleLoginChange} />
                 :
--- client/src/Components/NavBar/NavBarLoggedIn.js
+++ client/src/Components/NavBar/NavBarLoggedIn.js
@@ -4,22 +4,23 @@ const NavBarLoggedIn = ({ handleLoginChange }) => {
     const navigate = useNavigate();
     return (
         <div>
+            <div className="Spacer" />
             <button id="AboutButton" className="NavButton" type="button" onClick={() => navigate("/about")}>
                 About
-            </button>            
-           <button id="ContactButton" className="NavButton" type="button" onClick={() => navigate("/contact")}>
+            </button>
+            <button id="ContactButton" className="NavButton" type="button" onClick={() => navigate("/contact")}>
                 Contact
             </button>
             <button id="TrainingButton" className="NavButton" type="button" onClick={() => navigate("/trainings")}>
                 Trainings
-            </button>              
+            </button>
             <button id="ProfileButton" className="NavButton" type="button" onClick={() => null}>
                 Profile
             </button>
             <button id="LogOutButton" className="NavButton" type="button" onClick={() => {
                 handleLoginChange(false)
                 navigate('')
-                }}>
+            }}>
                 Log Out
             </button>
         </div>
