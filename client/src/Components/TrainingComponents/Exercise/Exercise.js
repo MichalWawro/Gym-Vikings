750f121b1cb653ed63d924bd7f268d0640e4e5c8
JakubTlak <marbixxd@gmail.com>
2023-12-07 12:31:55 +0100

    fetching changes

---
+++ client/src/Components/TrainingComponents/Exercise/Exercise.js
@@ -0,0 +1,30 @@
+import './Exercise.css'
+
+function Exercise({ exercise , index}) {
+
+    function capitalizeFirstLetter(inputString) {
+        return inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase();
+    }
+
+    return (
+        <div className="exercise" id={index}>
+            <h1>{exercise.name}</h1>
+            <h2>Type: {capitalizeFirstLetter(exercise.type)}</h2>
+            <h2>Level: {capitalizeFirstLetter(exercise.level)}</h2>
+            <div class="details">
+                <a>sets: {exercise.set}</a>
+                <a>reps: {exercise.reps}</a>
+                <a>weight: {exercise.weight} kg</a>
+            </div>
+            <div className='image-div'>
+            <h2>Muscles engaged by exercise:</h2>
+            <img className="image" id="img" src={exercise.img} />
+            </div>
+            <div className='video-div'>
+            <h2>Propper way of doing this exercise:</h2>
+            <img className="video" id="video" src={exercise.video} />
+            </div>
+        </div>
+    )
+}
+export default Exercise;
---
+++ client/src/Components/TrainingComponents/Training.js
@@ -0,0 +1,35 @@
+import React,{useEffect, useState} from "react"
+import Exercise from "./Exercise/Exercise";
+import './Training.css'
+
+function Training ({training, userId}){
+
+
+    function trainingDone() {
+        fetch(`http://localhost:8080/user/trainingDone?userId=${userId}&trainingId=${training.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then(res => res.json())
+        .catch(error => {
+            console.error('Error:', error);
+        });
+        
+    }
+
+
+    return(
+
+        <>
+        <h1 className="training-name">{training.name}</h1>
+        <h2 className="training-difficulty">Level: {training.level}</h2>
+        {training.exercises.map((exercise,index)=><Exercise exercise={exercise} index={index}/>)}
+        <buttton className="end-button" onClick={()=>trainingDone()}>Training Done</buttton>
+        </>
+
+    )
+}
+
+export default Training;
